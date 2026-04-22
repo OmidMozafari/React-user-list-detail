9940c9cad7fed95c3fb8a91f4695f6bcb674a8ac
OmidMozafari <omidmozafari444@gmail.com>
2026-04-22 23:43:02 +0430

    finally could completed the project in some hours

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { useState } from "react";
 export default function App(){
 
     const users = [
-        { id:1 ,name: "Ali", Age: 18,  bio: "Frontend Developer" },
-        { id:2 ,name: "Sara", Age: 26, bio: "UI Designer" },
-        { id:3 ,name: "Omid", Age: 23, bio: "Student" },
-        { id:3 ,name: "hamed", Age: 23, bio: "officer" },
-        { id:3 ,name: "naveed", Age: 30, bio: "manager" },
-        { id:3 ,name: "hamid", Age: 28, bio: "IT manager" },
-
-    
+        { id:1 ,name: "Ali", Age: 18,  bio: "Frontend Developer", photo:"/photos/ali.jfif" },
+        { id:2 ,name: "Sara", Age: 26, bio: "UI Designer", photo:"/photos/sara.jfif" },
+        { id:3 ,name: "Omid", Age: 23, bio: "Student", photo: "/photos/omid.jfif" },
+        { id:3 ,name: "hamed", Age: 23, bio: "officer", photo: "/photos/hamed.jfif" },
+        { id:3 ,name: "naveed", Age: 30, bio: "manager", photo: "/photos/n.jfif" },
+        { id:3 ,name: "hamid", Age: 28, bio: "IT manager", photo: "/photos/h.jfif" }    
     ]
     
     const [selectuser, setSelectUser] = useState([0])
@@ -20,13 +18,13 @@ export default function App(){
         
         }
         
+
         return <div className="container">
         
         <Usernames   handleselectedUser = {handleselectedUser} users={users} />
         <Userdetails handleselectedUser = {handleselectedUser} selectuser = {selectuser} />
-
-
     </div>
+    
     }
     
 
@@ -56,10 +54,20 @@ function Usernames({ handleselectedUser, users }) {
 
 function Userdetails({selectuser}){
     return <div className="userDetailsCard">
-                <h3 className="title">User Detail</h3>
-                <h2>{selectuser.name}</h2>
-                <p>{selectuser.Age}</p>
-                <p>{selectuser.bio}</p>
+
+                    <h3 className="title">User Detail</h3>
+
+                <div className="userdetailsCardContentsContainer">
+                    <div className="userDetailsCardContens">
+                        <h2>{selectuser.name}</h2>
+                        <p>{selectuser.Age}</p>
+                        <p>{selectuser.bio}</p>
+                    </div>
+
+                    <div className="imageContainer">
+                        <img src={selectuser.photo} />
+                    </div>
+                </div>
             </div>
 }
   
